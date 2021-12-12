9c770da663f5175a8cd995ac2b474b8d3031e56f
Kevan Png <pngk@seagroup.com>
2021-12-12 17:12:50 +0800

    add back commented out

--- kubernetes-installation.sh
+++ kubernetes-installation.sh
@@ -15,23 +15,23 @@ set -o xtrace
 # TODO change to yum install kind
 #brew install kind
 # TODO must run in root
-#curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
-#chmod +x ./kind
-#mv ./kind /usr/local/bin/kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
 
 # TODO add docker cli here
 
 ## add kubectl here
-#cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-#[kubernetes]
-#name=Kubernetes
-#baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-#enabled=1
-#gpgcheck=1
-#repo_gpgcheck=1
-#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-#EOF
-#yum install -y kubectl
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubectl
 
 # TODO change to multi node
 kind_cluster_exist=$(kind get clusters)
